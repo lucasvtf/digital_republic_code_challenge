e1b16f657d325cda6d8b7a9525827c193474b8aa
Lucas Victor <lucassvtf@gmail.com>
2024-05-26 18:31:20 -0300

    fix: Ajusta o componente Wall para lidar corretamente com as respostas da api.

--- frontend/src/components/Wall/Wall.tsx
+++ frontend/src/components/Wall/Wall.tsx
@@ -3,6 +3,8 @@ import { useForm, useFieldArray, SubmitHandler } from 'react-hook-form';
 import './Wall.css';
 import { FormValues } from './types';
 import ResponseBox from '../ResponseBox/ResponseBox';
+import { ErrorResponse, SuccessResponse } from '../ResponseBox/types';
+import { submitFormData } from '../../utils/api';
 
 const WallForm = () => {
   const { register, control, handleSubmit, reset } = useForm<FormValues>({
@@ -15,31 +17,24 @@ const WallForm = () => {
     name: 'walls',
   });
 
-  const [response, setResponse] = useState<{
-    message: string;
-    type: 'success' | 'error';
-  } | null>(null);
+  const [response, setResponse] = useState<
+    SuccessResponse | ErrorResponse | null
+  >(null);
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     try {
-      const result = await fetch('/api/submit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-      if (result.ok) {
-        const resultData = await result.json();
-        setResponse({ message: resultData.message, type: 'success' });
-        reset(); // Resetar o formulário após o envio bem-sucedido
-      } else {
-        const errorData = await result.json();
-        setResponse({ message: errorData.message, type: 'error' });
+      const responseData = await submitFormData(data);
+      setResponse(responseData);
+      if (!responseData.message) {
+        reset();
       }
     } catch (error) {
-      setResponse({ message: 'Erro ao enviar os dados.', type: 'error' });
+      setResponse({ message: 'Erro ao enviar os dados.' });
     }
+  };
+
+  const closeResponseBox = () => {
+    setResponse(null);
   };
 
   return (
@@ -84,34 +79,16 @@ const WallForm = () => {
                 })}
               />
             </label>
-            <button
-              type='button'
-              className='remove-btn'
-              onClick={() => remove(index)}
-            >
-              Remover parede
-            </button>
           </div>
         ))}
       </div>
       <div className='button-container'>
-        <button
-          type='button'
-          className='add-btn'
-          onClick={() => append({ height: 0, width: 0, doors: 0, windows: 0 })}
-        >
-          Adicionar parede
-        </button>
         <button type='submit' className='submit-btn'>
           Enviar
         </button>
       </div>
       {response && (
-        <ResponseBox
-          message={response.message}
-          type={response.type}
-          onClose={() => setResponse(null)}
-        />
+        <ResponseBox responseData={response} onClose={closeResponseBox} />
       )}
     </form>
   );
